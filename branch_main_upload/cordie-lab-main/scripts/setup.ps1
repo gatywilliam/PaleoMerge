807f5beecfde0e55f9db3176508f33f49e19b9aa
William Gaty <94122749+gatywilliam@users.noreply.github.com>
2024-07-27 12:31:17 -0500

    Add files via upload

---
+++ branch_main_upload/cordie-lab-main/scripts/setup.ps1
@@ -0,0 +1,7 @@
+# installs the necessary modules for the script to run
+python -m venv cordie_venv
+python activate cordie_venv
+pip install pandas
+pip install requests
+pip install openpyxl
+pip install pandasgui
